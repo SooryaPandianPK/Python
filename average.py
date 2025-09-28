ad15bb5f08e6754e5d42e07fe7f82ffd45ad432b
SooryaPandianPK <pandiansoorya@gmail.com>
2025-09-28 16:18:34 +0530

    Calculate average

---
+++ average.py
@@ -0,0 +1,9 @@
+#Author: Soorya Pandian
+def average(*arg:int):
+    #Given number of integers, this function will calculate their average
+    count=len(arg)
+    num=0
+    for i in arg:
+        num+=i
+    return num/count
+print("The average of the numbers is:",average(10,20,60))
