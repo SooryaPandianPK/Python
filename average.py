d8da06f6963bebc18d50d08a1f8cdaa59407f897
SooryaPandianPK <pandiansoorya@gmail.com>
2025-09-28 16:24:00 +0530

    Update average.py

--- average.py
+++ average.py
@@ -1,9 +1,10 @@
 #Author: Soorya Pandian
 def average(*arg:int):
-    #Given number of integers, this function will calculate their average
+    """Given number of integers, this function will calculate their average"""
     count=len(arg)
     num=0
     for i in arg:
         num+=i
     return num/count
 print("The average of the numbers is:",average(10,20,60))
+
